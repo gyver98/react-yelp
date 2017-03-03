4b5a1505fa18f1721c50044c869194238c669553
somdari <matthewchoi7@gmail.com>
2017-03-03 22:36:27 +1100

    Configuring Multiple Environments

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ class App extends React.Component{
   render() {
     return (
       <div className={styles.wrapper}> 
-        Hello React 
+        <h1>Environment: {__NODE_ENV__}</h1>
       </div>
     );
   }
